8802be7550af0cbd857f56df44d394e970f8b7b5
ynny <39652840+jlsbz@users.noreply.github.com>
2019-04-02 10:22:56 +0800

    Update semantic.bash

--- semantic.bash
+++ semantic.bash
@@ -1,4 +1,4 @@
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.2-complete.jar:./bin main.Main"
+export CCHK="java -classpath ./lib/antlr-4.7.2-complete.jar:./bin java.main.Main"
 $CCHK
