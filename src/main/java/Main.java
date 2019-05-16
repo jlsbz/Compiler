ce3afdf9db1b24a64be2e5ff146346ab57efba28
ynny <39652840+jlsbz@users.noreply.github.com>
2019-05-16 22:02:02 +0800

    千呼万唤始出来

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,12 +19,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         try {
-            //long startTime=System.currentTimeMillis();   //获取开始时间
-
             compile();
-            //long endTime=System.currentTimeMillis(); //获取结束时间
-            //System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
-
         } catch (Error error) {
             System.err.println(error.getMessage());
             System.exit(1);
